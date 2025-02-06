d7f877323c9623c36b47ac0c66427a3b6c1a58df
Aleksandr Kazantsev <Koper1331@yandex.ru>
2025-02-06 16:39:40 +0300

    add: Docker, Docker compose

--- README.md
+++ README.md
@@ -2,18 +2,28 @@
 Необходимо создать простое веб-приложение, которое позволяет пользователям искать репозитории на GitHub по имени пользователя и отображать информацию о найденных репозиториях. Используйте GitHub API для получения данных: GitHub Repositories API.
 
 ## Стэк
+- Vite
 - TypeScript
 - React
 - Redux Toolkit
 - framer-motion
 - CSS Modules
 - Axios
-- Jest
+- Docker, Docker Compose
 
-## Инструкция по использованию
+## Запуск приложения
+### Локально
 
-- Клонируйте репозиторий: ```git clone https://github.com/kersss71/github_search.git```  
+- Клонируйте репозиторий: ```git clone git@github.com:kersss71/github_search.git```  
 - Перейдите в папку проекта: ```cd <название папки>```
 - Установите зависимости: ```npm install```
-- Запустите проект: ```npm run dev```
-- Для запуска тестов: ```npm run test```
+- Запустите проект: ```npm run dev```  
+В браузере перейдите по адресу ```https://localhost:5173```  
+  
+### В Docker контейнере
+- Убедитесь, что Docker установлен и запущен
+- Создайте Docker образ и запустите контейнер:  
+```docker-compose build```  
+```docker-compose up```  
+В браузере перейдите по адресу ```https://localhost:3000```
+- Чтобы завершить: ```docker-compose down```
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,11 @@
 import { useSelector, useDispatch } from "react-redux";
 import { AppDispatch, RootState } from "../../store/store";
-import { fetchRepos } from "../../features/repositorySlice";
+import { loadRepositories } from "../../features/repositorySlice";
 import { FC } from "react";
 import cls from "./Home.module.css";
-import { SearchBar } from "../../components/Search/Search";
+import { Search } from "../../components/Search/Search";
 import InfiniteScroll from "react-infinite-scroll-component";
-import { RepoCard } from "../../components/RepositoryCard/RepositoryCard";
+import { RepositoryCard } from "../../components/RepositoryCard/RepositoryCard";
 
 export const Home: FC = () => {
   const dispatch = useDispatch<AppDispatch>();
@@ -15,16 +15,16 @@ export const Home: FC = () => {
 
   return (
     <div className={cls.contaier}>
-      <SearchBar />
+      <Search />
       {status === "loading" && <p>Загрузка...</p>}
       <InfiniteScroll
         dataLength={repos.length}
-        next={() => dispatch(fetchRepos({ username, page: page }))}
+        next={() => dispatch(loadRepositories({ username, page: page }))}
         hasMore={true}
         loader={<h4>Загрузка...</h4>}
       >
         {repos.map((repo) => (
-          <RepoCard key={repo.id} {...repo} />
+          <RepositoryCard key={repo.id} {...repo} />
         ))}
       </InfiniteScroll>
     </div>
